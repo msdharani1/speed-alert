adddaf7456a4f4f1302259043876ba40315a2bcd
msdharani1 <msdharaniofficail@gmail.com>
2024-01-01 10:56:48 +0530

    upload my speed alret project

---
+++ test.py
@@ -0,0 +1,17 @@
+QR=int(input('Current QR result: '))
+CS=int(input('Current Speed: '))
+
+if(CS>QR):
+    print('Please Go Slow, The Max speed limit is 30 KMs')
+elif(CS>QR):
+    print('Please Go Slow, The Max speed limit is 40 KMs')
+elif(CS>QR):
+    print('Please Go Slow, The Max speed limit is 60 KMs')
+elif(CS>QR):
+    print('Please Go Slow, The Max speed limit is 80 KMs')
+elif(CS>QR):
+    print('Please Go Slow, The Max speed limit is 100 KMs')
+    elif(CS>QR):
+    print('Please Go Slow, The Max speed limit is 120 KMs')
+else:
+    print('-')
